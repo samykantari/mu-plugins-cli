5d59d425e23f1d4ffa26b165c846f4a8e5cce30a
Kantari Samy <samy@whodunit.fr>
2022-12-19 08:35:14 +0100

    correction path & add delete

--- mu-plugins-cli.php
+++ mu-plugins-cli.php
@@ -11,47 +11,66 @@
  */
 
 // Only load this plugin once and bail if WP CLI is not present
-if (!defined('WP_CLI')) {
-    return;
+if ( ! defined( 'WP_CLI' ) ) {
+	return;
 }
 
 
-class Mu_plugins_CLI
-{
+class Mu_plugins_CLI {
 
-    public function path($args)
-    {
+	public function path( $args ) {
 
-        if (!defined('WPMU_PLUGIN_DIR')) {
-            WP_CLI::error('The constant "WPMU_PLUGIN_DIR" does not exist');
-        }
+		if ( ! defined( 'WPMU_PLUGIN_DIR' ) ) {
+			WP_CLI::error( 'The constant "WPMU_PLUGIN_DIR" does not exist' );
+		}
 
 
-        WP_CLI::line(WPMU_PLUGIN_DIR);
-    }
+		WP_CLI::line( WPMU_PLUGIN_DIR );
+	}
 
-    // create a new command list mu-plugins
-    public function list($args)
-    {
-        $mu_plugins = get_mu_plugins(); // details get_mu_plugins() https://developer.wordpress.org/reference/functions/get_mu_plugins/
+	// create a new command list mu-plugins
+	public function list( $args ) {
+		$mu_plugins = get_mu_plugins(); // details get_mu_plugins() https://developer.wordpress.org/reference/functions/get_mu_plugins/
 
-        if (empty($mu_plugins)) {
-            WP_CLI::error('No mu-plugins found');
-        }
+		if ( empty( $mu_plugins ) ) {
+			WP_CLI::error( 'No mu-plugins found' );
+		}
 
-        $mu_plugins = array_map(function ($mu_plugin) {
-            return [
-                'name' => $mu_plugin['Name'],
-                'path' => $mu_plugin['PluginURI'],
-            ];
-        }, $mu_plugins);
+		$mu_plugins = array_map( function ( $mu_plugin, $key ) {
+			return [
+				'name'        => $mu_plugin['Name'],
+				'name_delete' => $key,
+				'version'     => $mu_plugin['Version'],
+			];
+		}, $mu_plugins );
 
-        WP_CLI\Utils\format_items('table', $mu_plugins, ['name', 'path']);
-    }
+		WP_CLI\Utils\format_items( 'table', $mu_plugins, [ 'name', 'version' ] );
+	}
 
+	// delete file with $wp_filesystem
+	public function delete( $args ) {
+		global $wp_filesystem;
+
+		// check if the file exists
+		if ( ! file_exists( WPMU_PLUGIN_DIR . '/' . $args[0] ) ) {
+			WP_CLI::error( 'The file does not exist' );
+		}
+
+		// check if the file is writable
+		if ( ! is_writable( WPMU_PLUGIN_DIR . '/' . $args[0] ) ) {
+			WP_CLI::error( 'The file is not writable' );
+		}
+
+		// delete the file
+		if ( ! $wp_filesystem->delete( WPMU_PLUGIN_DIR . '/' . $args[0] ) ) {
+			WP_CLI::error( 'The file could not be deleted' );
+		}
+
+		WP_CLI::success( 'The file has been deleted' );
+	}
 
 
 }
 
 
-WP_CLI::add_command('mu-plugin', 'Mu_plugins_CLI');
+WP_CLI::add_command( 'mu-plugin', 'Mu_plugins_CLI' );
